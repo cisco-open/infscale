5bc5ce5047012e741803c1d4eced2fa88ac8f46e
Myungjin Lee <myungjin@users.noreply.github.com>
2023-12-22 15:45:30 -0800

    misc: fix typo (#9)

--- infscale/constants.py
+++ infscale/constants.py
@@ -1,6 +1,7 @@
-"""contrants."""
+"""constants."""
 
 # This file contains project-level constants.
-# Note: DO NOT ADD CONSTANTS SPECIFIC TO A SINGLE FILE
+# Note: DO NOT ADD CONSTANTS SPECIFIC TO A SINGLE FILE OR SUBMODULE
+
 CONTROLLER_PORT = 31310
 HEART_BEAT_PERIOD = 5  # 5 seconds; heart beat between controller and agent
