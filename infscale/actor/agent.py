76c0146f1c1ae20c13e22d98b6c8a5ff955f9cf5
raresgaia123 <137071040+raresgaia123@users.noreply.github.com>
2025-02-25 19:02:11 +0200

    fix: fix various issues with RPC messages and types (#113)
    Adjusted some messages that are being exchanged between agent and controller.
    Adjusted agent contexts resource types.
    Adjusted data type that is being saved in agent context, for this cpu.py was updated to be able to convert rpc message to stats class instance.
    Adjusted gpu.py type check, due to wrongly checking isinstance.
    Fixed GpuStat rpc message structure, due to default values not being sent with the message (https://protobuf.dev/programming-guides/proto3/#default)

--- infscale/actor/agent.py
+++ infscale/actor/agent.py
@@ -416,7 +416,7 @@ class Agent:
             gpu_msg_list = GpuMonitor.stats_to_proto(gpu_stats)
             vram_msg_list = GpuMonitor.stats_to_proto(vram_stats)
 
-            status_msg = pb2.ResourceStats()
+            status_msg = pb2.Status()
             status_msg.id = self.id
             status_msg.gpu_stats.extend(gpu_msg_list)
             status_msg.vram_stats.extend(vram_msg_list)
